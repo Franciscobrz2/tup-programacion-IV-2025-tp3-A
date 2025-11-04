67326d911c5127063ae08d7400f8ed0f5e23fa62
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-04 17:56:01 -0300

    se crea el app principal con su configuracion y rutas para utilizar el servidor.

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { conectarDB } from "./config/db.js";
+import usuarioRouter from "./modules/usuario/usuarioRoutes.js";
+conectarDB();
+
+const app = express();
+const port = 3000;
+
+//habilitar cors
+app.use(cors())
+// Para interpretar body como JSON
+app.use(express.json());
+
+app.use("/api/usuarios", usuarioRouter);
+app.use("/api/alumnos", usuarioRouter);
+app.use("/api/materias", usuarioRouter);
+app.use("/api/notas", usuarioRouter);
+
+
+
+//para recurso no existente
+app.use((req,res) => {
+  res.status(404).json({
+    succes: false,
+    message: `404 Not Found. Recurso (${req.method} ${req.hostname}${req.path})no encontrado.`
+  })
+})
+
+app.listen(port, () => {
+  console.log(`La aplicación esta funcionando en el puerto ${port}`);
+});
