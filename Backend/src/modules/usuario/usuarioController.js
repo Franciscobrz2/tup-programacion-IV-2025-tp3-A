aa3aa324741b64d38886a4964a08fc4631478b5c
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-05 14:09:01 -0300

    CRUD de usuario

---
+++ Backend/src/modules/usuario/usuarioController.js
@@ -0,0 +1,111 @@
+import { db } from "../../config/db.js";
+import bcrypt from "bcrypt";
+
+export async function getAllUsuarios (req, res) {
+    const filter = req.query.filter;
+    const [resultUsuario] = await db.execute("SELECT id, nombre, email FROM usuario");
+
+    res.json({
+        succes: true,
+        usuarios: resultUsuario
+    });
+
+}
+
+export async function getUsuariosById (req, res) {
+    const id = Number(req.params.id);
+
+    const [resultUsuario] = await db.execute("SELECT nombre, email FROM usuario WHERE id = ?" ,[
+        id
+    ]);
+
+    if(resultUsuario.length === 0){
+        return res.status(400).json({
+            success: false,
+            message: "No se encontro un usuario con ese id."
+        })
+    }
+
+    res.json({
+        succes: true,
+        usuarios: resultUsuario
+    })
+
+}
+
+export async function createUsuario (req, res) {
+    const { nombre, email, password } = req.body
+    
+    const password_hash = await bcrypt.hash(password, 9);
+
+    const [resultUsuario] = await db.execute("INSERT INTO usuario (nombre, email, password_hash) VALUES(?, ?, ?)" ,[
+        nombre, email, password_hash
+    ]);
+
+    if(resultUsuario.affectedRows === 0){
+        return res.status(400).json({
+            success: false,
+            message: "No se puedo crear el usuario."
+        });
+    }
+
+    res.json({
+        succes: true,
+        message: "Usuario creado creado con exito."
+    });
+
+}
+
+export async function updateUsuario (req, res) {
+    const id = Number(req.params.id);
+    const { nombre, email, password, nueva_password } = req.body;
+
+    let sql = "UPDATE usuario SET";
+    let parametro = [];
+    if(password && nueva_password){
+        sql += " password_hash = ? ";
+        const password_hash = await bcrypt.hash(nueva_password,9);
+        parametro.push(password_hash)
+    }else{
+        sql += " nombre = ?";
+        parametro.push(nombre)
+    }
+    sql += " WHERE id = ?" 
+
+    parametro.push(id)
+    const [resultUsuario] = await db.execute(sql, parametro);
+
+    if(resultUsuario.affectedRows === 0){
+        return res.status(400).json({
+            success: false,
+            message: "No se encontro un usuario con ese id."
+        });
+    }
+
+    res.json({
+        succes: true,
+        message: "Usuario actualizado con exito."
+    });
+
+}
+
+export async function deleteUsuario (req, res) {
+    const id = Number(req.params.id);
+
+    const [resultUsuario] = await db.execute("DELETE FROM usuario WHERE id = ?" ,[
+        id
+    ]);
+
+    if(resultUsuario.affectedRows === 0){
+        return res.status(400).json({
+            success: false,
+            message: "No se encontro un usuario con ese id."
+        });
+    }
+
+    res.json({
+        succes: true,
+        message: `Se elimino el usuario con el id ${id}.`
+    });
+
+}
