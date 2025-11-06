338feb9b3a6a7dde60b13e3d00b5b88ec50b7ace
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-06 14:12:11 -0300

    se agrega la ruta empaquetada de notas.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,7 @@ import { conectarDB } from "./config/db.js";
 import usuarioRouter from "./modules/usuario/usuarioRoutes.js";
 import alumnoRouter from "./modules/alumno/alumnoRoutes.js";
 import materiaRouter from "./modules/materia/materiaRoutes.js";
+import notaRouter from "./modules/nota/notasRoutes.js"
 conectarDB();
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use("/api/usuarios", usuarioRouter);
 app.use("/api/alumnos", alumnoRouter);
 app.use("/api/materias", materiaRouter);
-app.use("/api/notas", usuarioRouter);
+app.use("/api/notas", notaRouter);
 
 
 
