a748c8c2848fa6909963e149250ceea288a44505
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-11 23:48:34 -0300

    feat: agrgega rutas para pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,24 @@ import "@picocss/pico";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Layout } from "./components/Layout/Layout.jsx";
 import { HomePage } from './pages/homePage/HomePage';
+import { AuthProvider } from './Auth/Auth.jsx';
+import { AlumnosPage } from './pages/AlumnosPage/AlumnosPage.jsx';
+import { MateriasPage } from './pages/MateriasPage/MateriasPage.jsx';
+import { NotasPage } from './pages/NotasPage/NotasPage.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}/>
-        <Route index element={<HomePage/>}/>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  <AuthProvider>
+     <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}> 
+            <Route index element={<HomePage/>}/>
+            <Route path="/alumnos" element={<AlumnosPage/>}/>
+            <Route path="/materias" element={<MateriasPage/>}/>
+            <Route path="/notas" element={<NotasPage/>}/>
+          </Route>
+   
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>,
+  </AuthProvider>
 )
