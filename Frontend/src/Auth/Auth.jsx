89896e765e925b56ce32f64a73e37f477888e0d8
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-11 23:49:40 -0300

    feat: crea contexto para manejar estados de usuario

---
+++ Frontend/src/Auth/Auth.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { createContext, useContext } from "react";
+import { dominio } from "../utils/dominio";
+
+const authContext = createContext(null);
+
+export const useAuth = () => useContext(authContext);
+
+export const AuthProvider = ({children}) => {
+    const [token, setToken] = useState(null);
+    const [nombre, setNombre] = useState(null);
+    
+    const login = async (registro) => {
+        try{
+            const response = await fetch(`${dominio}/login`,{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(registro)
+            });
+
+            const session = await response.json();
+
+            if(!response.ok && response.status === 400){
+                throw new Error(session.error);
+            }
+
+            setToken(!session.token);
+            setNombre(session.nombre);
+            return { success: true};
+        }catch(err){
+            return { success: false };
+        }
+    }
+    
+    const logout = () => {
+        setToken(null);
+        setNombre(null);
+    };
+
+    const fetchAuth = async (url, options = {}) => {
+        return await fetch(url, 
+           { ...options},
+        )
+    }
+    return (
+        <authContext.Provider value={{
+            token, 
+            nombre, 
+            login, 
+            logout,
+            isAuthenticated: !!token,
+            fetchAuth,
+        }}>
+            {children}
+        </authContext.Provider>
+    );
+} 
