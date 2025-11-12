ce30b1cc130512b6492744023e60bb99e0a0de28
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-11 23:51:52 -0300

    feat: agrega links para navegar

--- Frontend/src/components/Layout/Layout.jsx
+++ Frontend/src/components/Layout/Layout.jsx
@@ -1,10 +1,31 @@
 import { Link, Outlet } from "react-router";
-
+import { useAuth } from "../../Auth/Auth";
 export const Layout = () => {
-
+    const {logout, isAuthenticated} = useAuth();
     return(
-        <main>
-
+        <main className="container">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/alumnos">Alumnos</Link>
+                    </li>
+                    <li>
+                        <Link to="/materias">Materias</Link>
+                    </li>
+                    <li>
+                        <Link to="/notas">Notas</Link>
+                    </li>
+                </ul>
+                <li>
+                    { isAuthenticated  && 
+                        <button onClick={() => logout()}>Salir</button>
+                    }
+                </li>
+            </nav>
+            <Outlet/>
         </main>
     );
 }
