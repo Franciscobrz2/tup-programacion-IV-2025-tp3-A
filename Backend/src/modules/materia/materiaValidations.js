1a8dfd02912ae49e1ea24779d59fc5c1f21280d9
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 13:51:08 -0300

    feat(api): agrega mas validaciones a materia

--- Backend/src/modules/materia/materiaValidations.js
+++ Backend/src/modules/materia/materiaValidations.js
@@ -1,17 +1,21 @@
 import { body } from "express-validator";
 
-const formatearFecha = (date) =>{
-    
-}
 export const validarMateria = [
     body('nombre')
         .isAlpha("es-ES", {ignore: " "})
-        .withMessage('El nombre no puede estar vacio y no puede contener numeros.'),
+        .withMessage('No puede contener numeros.')
+        .isLength({max: 20})
+        .withMessage('No puede exceder los 20 caracteres.'),
     body('codigo')
         .isAlphanumeric()
-        .withMessage('El codigo debe ser alfanumerico y sin espacios.'),
+        .withMessage('El codigo debe ser alfanumerico y sin espacios.')
+        .isLength({max: 20})
+        .withMessage('No puede exceder los 20 caracteres.'),
     body('año')
-        .isDate({delimiters:['/','-']}).withMessage('El formato de la fecha debe ser YYYY/MM/DD.'),
-  
+        .isInt({min: 1})
+        .withMessage('El año debe ser un numero entero positivo.')
+        .isLength({min:4, max:4})
+        .withMessage('El año debe tener 4 digitos.'),
+
 ]
 
