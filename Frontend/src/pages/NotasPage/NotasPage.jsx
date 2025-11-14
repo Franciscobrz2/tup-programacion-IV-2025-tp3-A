cea8dddb1546f39f30cd0e54563a6e21f071ed03
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 14:32:28 -0300

    refactor(front):  saca split en año y simplifica el reduce

--- Frontend/src/pages/NotasPage/NotasPage.jsx
+++ Frontend/src/pages/NotasPage/NotasPage.jsx
@@ -1,13 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router";
 import useAlumnos from "../../api/alumnos";
 import useMaterias from "../../api/materias";
 import useNotas from "../../api/notas";
 import Tabla from "../../components/Tabla/Tabla";
+import { useAuth } from "../../Auth/Auth";
+import { dominio } from "../../utils/dominio";
 
 export const NotasPage = () => {
-    const { getAlumnos, postAlumnos, putAlumnos, deleteAlumnos } = useAlumnos();
+    const url = `${dominio}/notas`
+    const { getAlumnos } = useAlumnos();
     const { getMaterias } = useMaterias();
     const { getNotas } = useNotas();
+    const { fetchAuth } = useAuth();
 
     const [ listaAlumnos, setListaAlumnos ] = useState([]);
     const [ listaMaterias, setListaMaterias ] = useState([]);
@@ -24,7 +29,7 @@ export const NotasPage = () => {
                 dni: alumno?.dni,
                 materia: materia?.nombre,
                 codigo: materia?.codigo,
-                año: materia?.año.split("-")[0],
+                año: materia?.año,
                 promedio: (
                     (Number(n?.nota1) + 
                     Number(n?.nota2) +
@@ -35,44 +40,44 @@ export const NotasPage = () => {
         return n
     });
     
+    const fetchParalelo = useCallback(async () => {
+        try{
+            const [nota, alumno, materia] = await Promise.all([
+                getNotas(),
+                getAlumnos(),
+                getMaterias(),
+            ])
+
+            if(!nota.success || !alumno.success || !materia.success ){
+                return console.error("Error al obtener datos.",nota, alumno, materia)
+            }
+            //Estado para las columnas y saco los id, nombre y codigo en este caso
+            setColumnas( () =>{
+                const colum =  nota.columnasNota
+                .concat(alumno?.columnasAlumno , materia?.columnasMateria)
+                //Se saca lo repetido 
+                .filter(c => c !== "id" && c !== "nombre" && c !== "codigo")
+                //saca el _id
+                .map(item => item.split("_")[0])
+                colum.push("promedio")
+                //se ordena alfabeticamente
+                colum.sort()
+                //se pushea id primero
+                colum.splice(0,0,"id")
+                return colum
+            })
+            
+            setListaAlumnos(alumno.alumnos)
+            setListaMaterias(materia.materias)
+            setListaNotas(nota.notas)
+        }catch(err){
+            console.error("Error al cargar datos", err);
+        }
+    },[])
     
     useEffect(()=>{
-        const fetchParalelo = async () => {
-            try{
-                const [nota, alumno, materia] = await Promise.all([
-                    getNotas(),
-                    getAlumnos(),
-                    getMaterias(),
-                ])
-                console.log("api", nota)
-                if(!nota.success || !alumno.success || !materia.success ){
-                    return console.error("Error al obtener datos.",nota, alumno, materia)
-                }
-                //Estado para las columnas y saco los id, nombre y codigo en este caso
-                setColumnas( () =>{
-                    const colum =  nota.columnasNota
-                    .concat(alumno?.columnasAlumno , materia?.columnasMateria)
-                    //Se saca lo repetido 
-                    .filter(c => c !== "id" && c !== "nombre" && c !== "codigo")
-                    //saca el _id
-                    .map(item => item.split("_")[0])
-                    colum.push("promedio")
-                    //se ordena alfabeticamente
-                    colum.sort()
-                    //se pushea id primero
-                    colum.splice(0,0,"id")
-                    return colum
-                })
-                
-                setListaAlumnos(alumno.alumnos)
-                setListaMaterias(materia.materias)
-                setListaNotas(nota.notas)
-            }catch(err){
-                console.error("Error al cargar datos", err);
-            }
-        } 
         fetchParalelo();
-    },[]);
+    },[fetchParalelo]);
     
    
     
@@ -80,12 +85,26 @@ export const NotasPage = () => {
         [...acc,{key: col, th: col.charAt(0).toUpperCase() + col.slice(1)}]
     ,[])
 
-    if(!listaNotas.length) return <h2>No existen notas para mostrar</h2>
+    const handleQuitar = async (id) => {
+        if (window.confirm("¿Desea quitar el notas?")) {
+        const response = await fetchAuth(`${url}/${id}`, {
+            method: "DELETE",
+        });
+        const data = await response.json();
+
+        if (!response.ok || !data.success) {
+            return window.alert("Error al quitar notas");
+        }
+
+        await fetchParalelo();
+        }
+    }
 
     return(
-        <>
+        <>  
             <h1>lista de notas de alumnos</h1>
-            <Tabla columnas={notasColumnas} lista={alumnosJoin}/>
+            
+            <Tabla columnas={notasColumnas} lista={alumnosJoin} handleQuitar={handleQuitar}/>
         </>
     );
 }
