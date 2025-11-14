4f6f54274386f5da3939a7c8a719560d2e055672
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 14:23:32 -0300

    feat(front): agrega visualizacion para validaciones en Select

--- Frontend/src/components/Select/Select.jsx
+++ Frontend/src/components/Select/Select.jsx
@@ -1,15 +1,29 @@
 
-export const Select = ({lista,keys}) => {
+export const Select = ({lista,keys, onChange, isSelected, error}) => {
 
     return(
         <>
-           <select key={keys}>
+            <select 
+                key={keys}
+                onChange={onChange}
+                aria-invalid={!!error}
+                style={{
+                    borderColor: error ? "red" : undefined
+                }}
+            >
+                <option value="" hidden >{isSelected}</option>
                 {lista.map( item => (
-                    <option key={item.id} value={item.id}>
+                    <option 
+                        key={item.id} 
+                        value={item.id}
+                    >
                         {item.nombre} {item?.apellido}
                     </option>
                 ))}
-           </select>
+            </select>
+            {error && (
+                <small style={{ color: "red" }}>{error}</small>
+            )}
         </>
     );
 }
