20d461ef28e7591d4380d4ee091ed5f370e685fa
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 14:11:12 -0300

    fix(tabla): corrige las restricciones de las fk para eliminar materia y alumno

--- Backend/src/models/nota.sql
+++ Backend/src/models/nota.sql
@@ -8,6 +8,6 @@ CREATE TABLE `nota` (
   PRIMARY KEY (`id`),
   KEY `nota_materia_fk2_idx` (`materia_id`),
   KEY `nota_alumno_fk1_idx` (`alumno_id`),
-  CONSTRAINT `nota_alumno_fk1` FOREIGN KEY (`alumno_id`) REFERENCES `alumno` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `nota_materia_fk2` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `nota_alumno_fk1` FOREIGN KEY (`alumno_id`) REFERENCES `alumno` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `nota_materia_fk2` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 )
