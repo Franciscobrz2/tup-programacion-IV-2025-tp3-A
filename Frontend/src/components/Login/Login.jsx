04e4d2e33ea18fb32dd2a52f99cc92f4265e32c6
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 20:33:35 -0300

    feat(front): agrega funcionalidad a Login

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,29 +1,93 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import { useAuth } from "../../Auth/Auth";
+import { useNavigate } from "react-router";
 
 export const Login = () => {
-
+    const navigator = useNavigate();
+    const valoresIniciales = {
+        email: "",
+        password: "",
+    }
+    const [values, setValues] = useState(valoresIniciales);
+    const { error, login } = useAuth();
+    
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setValues( (p) => (
+        {...p, 
+            [name]: value
+        }
+        ))
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("envia", values)
+        const verificar = await login(values)
+        console.log("s",verificar)
+        if(verificar.success){
+            navigator("/")     
+        }
+        
+    };
+    console.log("->>>",error)
     return (
         <>
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="email">Email</label>
                     <input 
+                        required
                         type="email" 
                         name="email"  
-                        value={""}
-                        onChange={null}
+                        value={values?.email}
+                        onChange={handleChange}
                         className=""
                         placeholder="@ejemplo.com"
+                        aria-invalid={
+                            error  && error.some((e) => e?.path === "email")
+                        }
                     />
-                    <br />
+                    {error && (
+                        <small>
+                            {error
+                            .filter((e) => e?.path === "email")
+                            .map((e) => e?.msg)
+                            .join(", ")}
+                        </small>
+                    )}
                     <label htmlFor="password">Contraseña</label>
                     <input 
+                        required
                         type="password" 
                         name="password"  
-                        value={""}
-                        onChange={null}
+                        value={values?.password}
+                        onChange={handleChange}
                         className=""
-                        placeholder="contraseñas"
+                        placeholder="contraseña"
+                        aria-invalid={
+                            error && error.some((e) => e?.path === "password")
+                        }
                     />
+                    {error && (
+                        <small>
+                            {error
+                            .filter((e) => e?.path === "password")
+                            .map((e) => e?.msg)
+                            .join(", ")}
+                        </small>
+                    )}
+                    {error && (
+                        <small style={{color:"red"}}>
+                            {error
+                            .map((e) => e?.session)
+                            }
+                        </small>
+                    )}
+                    <label>No tienes cuenta?</label>
+                    <Link to="/register">
+                        Crear ahora
+                    </Link>
                     <button type="submit">Iniciar sesion</button>
                 </form>
 
