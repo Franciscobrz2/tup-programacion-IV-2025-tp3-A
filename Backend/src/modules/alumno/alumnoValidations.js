2549b0140a0364236e58a05d3f19108c94506979
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 13:50:00 -0300

    feat(api): agrega mas validaciones

--- Backend/src/modules/alumno/alumnoValidations.js
+++ Backend/src/modules/alumno/alumnoValidations.js
@@ -4,14 +4,16 @@ import { db } from "../../config/db.js";
 export const validarAlumno = [
     body('nombre')
         .isAlpha("es-ES", {ignore: " "})
-        .withMessage('El nombre no puede estar vacio y no puede contener numeros.'),
+        .withMessage('El nombre no puede estar vacio y no puede contener numeros.')
+        .isLength({max:20}).withMessage('El nombre no debe exceder los 20 caracterers'),
     body('apellido')
         .isAlpha("es-ES", {ignore: " "})
-        .withMessage('El apellido no puede estar vacio y no puede contener numeros.'),
+        .withMessage('No puede contener numeros.')
+        .isLength({max:20}).withMessage('El nombre no debe exceder los 20 caracterers'),
     body('dni')
-        .isLength({min:8, max:8})
         .isInt({min:0})
-        .withMessage('El DNI solo debe ser una cadena de numeros sin puntos.')
+        .withMessage('El DNI debe ser un numero entero positivo.')
+        .isLength({min:8, max:8}).withMessage('El DNI debe tener 8 digitos y sin puntos.')
         .custom(async(dni, {req}) =>{
             console.log(req.params.id)
             if(req.params.id){
