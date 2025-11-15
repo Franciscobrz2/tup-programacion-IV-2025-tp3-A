2a1c54b1269902e47dd23cf0223609b444f15b93
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 21:09:15 -0300

    feat(api): agrega verificacion de autenticacion en notas

--- Backend/src/modules/nota/notasRoutes.js
+++ Backend/src/modules/nota/notasRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import { getAllNotas, getNotaById, createNota, updateNota, deleteNota} from "./notasController.js";
 import { validarId, verificarValidacion} from "../../utils/verificarValidacion.js";
 import { validarNota } from "./notasValidations.js";
-
+import { verificarAutenticacion } from "../auth/auth.js";
 const router = express.Router();
 
-router.get("/", getAllNotas);
-router.get("/:id", validarId("id"), verificarValidacion, getNotaById);
-router.post("/", validarNota, verificarValidacion, createNota);
-router.put("/:id", validarNota, validarId("id"), verificarValidacion, updateNota);
-router.delete("/:id", validarId("id"), verificarValidacion, deleteNota);
+router.get("/", verificarAutenticacion, getAllNotas);
+router.get("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, getNotaById);
+router.post("/", verificarAutenticacion, validarNota, verificarValidacion, createNota);
+router.put("/:id", verificarAutenticacion, validarNota, validarId("id"), verificarValidacion, updateNota);
+router.delete("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, deleteNota);
 
 export default router;
