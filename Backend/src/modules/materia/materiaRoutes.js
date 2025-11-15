c041ac42b1c03b47f9862b88b54358e54619ffd9
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 21:09:01 -0300

    feat(api): agrega verificacion de autenticacion en materias

--- Backend/src/modules/materia/materiaRoutes.js
+++ Backend/src/modules/materia/materiaRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import { getAllMaterias, getMateriaById, createMateria, updateMateria, deleteMateria} from "./materiaController.js";
 import { validarId, verificarValidacion} from "../../utils/verificarValidacion.js";
 import { validarMateria } from "./materiaValidations.js";
-
+import { verificarAutenticacion } from "../auth/auth.js";
 const router = express.Router();
 
-router.get("/", getAllMaterias);
-router.get("/:id", validarId("id"), verificarValidacion, getMateriaById);
-router.post("/", validarMateria, verificarValidacion, createMateria);
-router.put("/:id", validarMateria, validarId("id"), verificarValidacion, updateMateria);
-router.delete("/:id", validarId("id"), verificarValidacion, deleteMateria);
+router.get("/", verificarAutenticacion, getAllMaterias);
+router.get("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, getMateriaById);
+router.post("/", verificarAutenticacion, validarMateria, verificarValidacion, createMateria);
+router.put("/:id", verificarAutenticacion, validarMateria, validarId("id"), verificarValidacion, updateMateria);
+router.delete("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, deleteMateria);
 
 export default router;
