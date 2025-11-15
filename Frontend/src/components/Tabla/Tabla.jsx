d67d2f293e80327b3b51e341276198a5a854b1c7
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 22:48:00 -0300

    feat(front): agrega componente Quitar a las filas

--- Frontend/src/components/Tabla/Tabla.jsx
+++ Frontend/src/components/Tabla/Tabla.jsx
@@ -1,46 +1,47 @@
 import { Link, useLocation } from "react-router";
+import { Quitar } from "../Quitar/Quitar";
+import "./Tabla.css";
 
 export default function Tabla({ columnas, lista, handleQuitar }) {
-  const {pathname} = useLocation();
-  console.log("->",location.href)
-  const ruta = `http://localhost:5173${pathname}`
+  const { pathname } = useLocation();
+  const ruta = `http://localhost:5173${pathname}`;
 
   return (
+    <div className="tabla-container">
 
-    <>
-
-      <Link type="button" to={`${ruta}/crear`}>
-        {pathname.at(-2) === "a" ? "Nuevas":"Nuevos" } {pathname.split("/")}
+      <Link type="button" className="tabla-crear-btn" to={`${ruta}/crear`}>
+        {pathname.at(-2) === "a" ? "Nuevas" : "Nuevos"} {pathname.split("/")}
       </Link>
-      <table>
+
+      <table className="tabla">
         <thead>
           <tr>
-            {columnas.map(col => (
+            {columnas.map((col) => (
               <th key={col.key}>{col.th}</th>
             ))}
           </tr>
         </thead>
+
         <tbody>
           {lista.map((item) => (
             <tr key={item.id}>
-              {columnas.map(col => (
-                <td key={col.key}>{item[col.key]} 
-                  
-                </td>
+              {columnas.map((col) => (
+                <td key={col.key}>{item[col.key]}</td>
               ))}
-              <td>
+
+              <td className="tabla-acciones">
                 <Link role="button" to={`${ruta}/${item.id}/modificar`}>
                   Modificar
                 </Link>
-                
-                { pathname !== "/notas" && 
-                  <button onClick={() => handleQuitar(item.id)}>Quitar</button>
-                }                  
+
+                {pathname !== "/notas" && (
+                  <Quitar id={item.id} handleQuitar={handleQuitar} datos={item.nombre} />
+                )}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
