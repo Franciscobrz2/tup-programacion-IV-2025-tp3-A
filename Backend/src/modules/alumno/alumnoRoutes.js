0aac4d887001d299dab32a1be17848433b37d0ce
Franciscobrz2 <brzfrancisco717@gmail.com>
2025-11-14 21:08:32 -0300

    feat(api): agrega verificacion de autenticacion en alumnos

--- Backend/src/modules/alumno/alumnoRoutes.js
+++ Backend/src/modules/alumno/alumnoRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import { getAllAlumnos, getAlumnosById, createAlumno, updateAlumno, deleteAlumno} from "./alumnoController.js";
 import { validarId, verificarValidacion} from "../../utils/verificarValidacion.js";
 import { validarAlumno } from "./alumnoValidations.js";
-
+import { verificarAutenticacion } from "../auth/auth.js";
 const router = express.Router();
 
-router.get("/", getAllAlumnos);
-router.get("/:id", validarId("id"), verificarValidacion, getAlumnosById);
-router.post("/", validarAlumno, verificarValidacion, createAlumno);
-router.put("/:id", validarAlumno, validarId("id"), verificarValidacion, updateAlumno);
-router.delete("/:id", validarId("id"), verificarValidacion, deleteAlumno);
+router.get("/", verificarAutenticacion, getAllAlumnos);
+router.get("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, getAlumnosById);
+router.post("/", verificarAutenticacion, validarAlumno, verificarValidacion, createAlumno);
+router.put("/:id", verificarAutenticacion, validarAlumno, validarId("id"), verificarValidacion, updateAlumno);
+router.delete("/:id", verificarAutenticacion, validarId("id"), verificarValidacion, deleteAlumno);
 
 export default router;
